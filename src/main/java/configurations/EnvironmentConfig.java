9053b2a660af30417589db4ab7410d67d3ab5626
gaikwadakshay14 <gaikwadakshay504@gmail.com>
2022-03-13 14:53:03 +0530

    new project added

---
+++ src/main/java/configurations/EnvironmentConfig.java
@@ -0,0 +1,9 @@
+package configurations;
+
+public class EnvironmentConfig {
+	
+	
+           public static String appTestUrl= "www.kite.zerodha.com";
+           public static String appQaUrl= "www.qa.zerodha.com";
+           public static String appSITUrl= "www.sit.zerodha.com";
+}
